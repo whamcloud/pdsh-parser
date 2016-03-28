385479ef25ec940bee6c7ca2001c891138a70474
Wayne Kenneth Seymour III <wayne.k.seymour.iii@intel.com>
2016-03-28 14:56:41 -0400

    HYD-5726 add coverage to pdsh-parser
    Now that we have code coverage generation in CI,
    we can build upon this metric. We need our transpiled,
    ES6 code to also be covered accurately.
    
    We are going to need one for the GUI,
    and one for NodeJS projects.
    
     - Updated for coverage.
    
    Signed-off-by: Wayne Kenneth Seymour III <wayne.k.seymour.iii@intel.com>

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 rm -rf dist
-babel -d dist "source/**/*.js" "test/**/*.js"
+
+babel -d dist "test/**/*.js"
+
+if [[ $1 == "cover" ]]; then
+  # Instrumenting the src file.
+  babel -d dist "./source/**/*.js" --plugins __coverage__
+else
+  babel -d dist "./source/**/*.js"
+fi
 
 for i in $( find ./source -type f ); do
   cp $i ./dist/$i.flow
--- run_test.sh
+++ run_test.sh
@@ -13,6 +13,7 @@ do
     nvm use $node_version
     rm -rf node_modules
     npm i
-    npm t
+    npm run cover -- --reporter=cobertura
+    mv ./coverage/cobertura-coverage.xml ../coverage
     mv pdsh-parser-results*.xml ../results
 done
